f3200af5b545de6d54c7ef0386effd1e5a520e30
Melissa Nicholas <melissanicholas@Melissas-MacBook-Pro.local>
2025-06-24 22:20:39 -0700

    Fix DAG for Airflow 3: updated schedule parameter

--- dags/hello_world_dag.py
+++ dags/hello_world_dag.py
@@ -8,7 +8,7 @@ def say_hello():
 with DAG(
     dag_id="hello_world_dag",
     start_date=datetime(2023, 1, 1),
-    schedule_interval="@daily",
+    schedule="@daily",
     catchup=False,
     tags=["example"]
 ) as dag:
